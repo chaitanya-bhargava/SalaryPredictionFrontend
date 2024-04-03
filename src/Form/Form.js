024f43b0080f4eaad752423ccc4fae69535d4ce1
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-04-03 23:16:55 +0530

    loading added

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,70 +1,81 @@
-import React, { useState } from 'react';
-import './Form.css'
-import { TextField, Button, MenuItem, FormControl, InputLabel, Select, Typography } from '@mui/material';
+import React, { useState } from "react";
+import "./Form.css";
+import {
+  TextField,
+  Button,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+  Typography,
+} from "@mui/material";
+
+import { RotatingLines } from "react-loader-spinner";
 
 const FormComponent = () => {
-  const [age, setAge] = useState('');
-  const [experience, setExperience] = useState('');
-  const [gender, setGender] = useState('');
-  const [educationLevel, setEducationLevel] = useState('');
-  const [responseText, setResponseText] = useState('');
+  const [age, setAge] = useState("");
+  const [experience, setExperience] = useState("");
+  const [gender, setGender] = useState("");
+  const [educationLevel, setEducationLevel] = useState("");
+  const [responseText, setResponseText] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     var newGender;
     var newEducationLevel;
-    switch(gender){
-        case 'Male':
-            newGender='0'
-            break;
-        case 'Female':
-            newGender='1'
-            break;
-        default:
-            newGender='0'
-            break;
+    switch (gender) {
+      case "Male":
+        newGender = "0";
+        break;
+      case "Female":
+        newGender = "1";
+        break;
+      default:
+        newGender = "0";
+        break;
     }
-    switch(educationLevel){
-        case "Bachelor's":
-            newEducationLevel='0'
-            break;
-        case "Master's":
-            newEducationLevel='1'
-            break;
-        case "Phd":
-            newEducationLevel='2'
-            break;
-        default:
-            newEducationLevel='0'
-            break;
+    switch (educationLevel) {
+      case "Bachelor's":
+        newEducationLevel = "0";
+        break;
+      case "Master's":
+        newEducationLevel = "1";
+        break;
+      case "Phd":
+        newEducationLevel = "2";
+        break;
+      default:
+        newEducationLevel = "0";
+        break;
     }
     // Prepare query parameters
     const queryParams = new URLSearchParams({
       age: age,
       yearsExperience: experience,
       gender: newGender,
-      educationLevel: newEducationLevel
+      educationLevel: newEducationLevel,
     }).toString();
 
     // Construct URL with query parameters
     const url = `https://salarymodel.onrender.com//predict?${queryParams}`;
 
     try {
+      setResponseText("Loading...");
       const response = await fetch(url);
       const data = await response.json();
-      const prediction=data[0].prediction
-      const newPrediction=Math.trunc(prediction)
+      const prediction = data[0].prediction;
+      const newPrediction = Math.trunc(prediction);
       setResponseText(`Your salary will be around ₹${newPrediction} per month`);
     } catch (error) {
-      console.error('Error:', error);
-      setResponseText('Error fetching data');
+      console.error("Error:", error);
+      setResponseText("Error fetching data");
     }
   };
 
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <h1>Salary Prediction</h1>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit}>
         <TextField
           label="Age"
           variant="outlined"
@@ -110,18 +121,26 @@ const FormComponent = () => {
           type="text"
           required
         />
-        <Button type="submit" variant="contained" color="primary" sx={{marginTop:"20px", textAlign:"center"}}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ marginTop: "20px", textAlign: "center" }}
+        >
           Submit
         </Button>
       </form>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
       {responseText && (
-        <div>
-          <Typography variant="h5" textAlign={'center'}>
+        <div className="response">
+          <Typography variant="h5" textAlign={"center"}>
             {responseText}
           </Typography>
+          {responseText === "Loading..." && (
+            <RotatingLines strokeColor="#000000" width="50" strokeWidth="2"/>
+          )}
         </div>
       )}
     </div>
